7b87d2b2f7941d810832c86bb2fcabb3bb2d6d98
TZ029887 <tony.zhang@cerner.com>
2014-04-25 14:32:21 -0500

    aa

---
+++ project/Build.scala
@@ -0,0 +1,15 @@
+import sbt._
+import Keys._
+
+object WarmUpBuild extends Build {
+  
+  val appName = "warm-up"
+  val appVersion = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
+	
+  val main = PlayProject(appName, appVersion, appDependencies)
+	
+}
